2f23ffb2b793290dde6820a9012d10d878df1cf1
Виктор Артёмов <78853114+ViktorAtremov@users.noreply.github.com>
2022-03-02 19:20:46 +0700

    Update Fibonacci.java

--- src/ylab_viktor/additional_task/Fibonacci.java
+++ src/ylab_viktor/additional_task/Fibonacci.java
@@ -2,6 +2,22 @@ package src.ylab_viktor.additional_task;
 
 public class Fibonacci {
     public static void main(String[] args) {
+          int fib1 = 1;
+         int fib2 = 1;
+
+         int i = 0;
+        System.out.println("Введите число элемента: ");
+        Scanner sr = new Scanner(System.in);
+        int n = sr.nextInt();
+
+        while (i < n - 2 ) {
+            int fib3 = fib1 + fib2;
+            fib1 = fib2;
+            fib2 = fib3;
+            i += 1;
+        }
+        sr.close();
+        System.out.println("Результат: " + fib2);
 
     }
 }
