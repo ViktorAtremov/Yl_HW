fad8bc84e203dcd6ea9833e6996fedb6de49669e
Виктор Артёмов <78853114+ViktorAtremov@users.noreply.github.com>
2022-03-09 16:11:35 +0700

    update

--- src/Lesson_5/Cat.java
+++ src/Lesson_5/Cat.java
@@ -1,10 +1,12 @@
 package src.Lesson_5;
 
 public class Cat {
-    String name; //имя
+    private String name; //имя
     int age; // возраст
     int weight; // вес
     int strength; // сила
+    String color;
+    String adress;
 
    public boolean fight(Cat anotherCat) {
 
@@ -23,6 +25,33 @@ public class Cat {
             }
        return false;
    }
+
+
+    public void initialize(String name) {
+        this.name = name;
+    }
+    public void initialize(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public void initialize(String name, int age, int weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+    public void initialize(int weight,String color) {
+       this.weight = weight;
+       this.color = color;
+    }
+    public void initialize(String name, int age, int weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.color = color;
+        this.adress = adress;
+    }
+
+
 
 }
 
